bdb32bfe5ab50ee171c4119256f6d177986bc99f
devrereg <devrereg@gmail.com>
2025-04-23 23:28:40 +0900

    first commit

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from app.sqs_consumer import start_consumer_in_background
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/test")
+async def test():
+    return {"message": "test"}
+
+
+# ▶️ 앱 시작 시 자동으로 실행되는 이벤트 핸들러
+@app.on_event("startup")
+def startup_event():
+    # ▶️ 백그라운드에서 SQS consumer 시작
+    start_consumer_in_background()
---
+++ app/sqs_consumer.py
@@ -0,0 +1,47 @@
+import os
+import threading
+import boto3
+import json
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+QUEUE_URL = os.getenv('QUEUE_URL')
+REGION = os.getenv('REGION')
+
+def crawl(url: str):
+    print(f"[크롤링 시작] URL: {url}")
+    # selenium 작업을 여기에 연결하면 됨
+    time.sleep(2)
+    print(f"[크롤링 완료] URL: {url}")
+
+def poll_sqs():
+    print("SQS consumer 시작됨")
+    sqs = boto3.client("sqs", region_name=REGION)
+
+    while True:
+        response = sqs.receive_message(
+            QueueUrl=QUEUE_URL,
+            MaxNumberOfMessages=2,
+            WaitTimeSeconds=10
+        )
+        messages = response.get("Messages", [])
+        print(f"messages: {messages}")
+        instagram_ids = []
+        for message in messages:
+            body = json.loads(message["Body"])
+            id = body.get("instagramId")
+            instagram_ids.append(id)
+            if id:
+                crawl(id)
+
+            # 메시지 삭제
+            sqs.delete_message(
+                QueueUrl=QUEUE_URL,
+                ReceiptHandle=message["ReceiptHandle"]
+            )
+
+def start_consumer_in_background():
+    thread = threading.Thread(target=poll_sqs, daemon=True)
+    thread.start()

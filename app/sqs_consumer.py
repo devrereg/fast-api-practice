6a6aeb3f51b786080f2d4bc9459861b3da610f9f
devrereg <devrereg@gmail.com>
2025-04-26 22:41:56 +0900

    배포 테스트

--- app/sqs_consumer.py
+++ app/sqs_consumer.py
@@ -9,6 +9,10 @@ load_dotenv()
 
 QUEUE_URL = os.getenv('QUEUE_URL')
 REGION = os.getenv('REGION')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
+
 
 def crawl(url: str):
     print(f"[크롤링 시작] URL: {url}")
@@ -16,9 +20,15 @@ def crawl(url: str):
     time.sleep(2)
     print(f"[크롤링 완료] URL: {url}")
 
+
 def poll_sqs():
     print("SQS consumer 시작됨")
-    sqs = boto3.client("sqs", region_name=REGION)
+    sqs = boto3.client(
+        "sqs",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=REGION
+    )
 
     while True:
         response = sqs.receive_message(
@@ -41,6 +51,7 @@ def poll_sqs():
                 QueueUrl=QUEUE_URL,
                 ReceiptHandle=message["ReceiptHandle"]
             )
+
 
 def start_consumer_in_background():
     thread = threading.Thread(target=poll_sqs, daemon=True)
